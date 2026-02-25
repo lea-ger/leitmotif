e9a48354176e7d0f2d47ed5d88698c0389bdb00a
leanderg <leander.gerwing@gmail.com>
2026-02-26 00:50:33 +0100

    feat: add ssl for sensor tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    basicSsl(),
   ],
   server: {
+    https: true,
     allowedHosts: ['localhost', 'exploratively-older-tressa.ngrok-free.dev'],
   }
 })
